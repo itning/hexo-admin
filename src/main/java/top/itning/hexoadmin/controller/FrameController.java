6294bb4b5d553c5d6b0934bcca6b00a9bbd21b7a
itning <itning@itning.top>
2018-07-29 14:40:58 +0800

    add template

--- src/main/java/top/itning/hexoadmin/controller/FrameController.java
+++ src/main/java/top/itning/hexoadmin/controller/FrameController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import top.itning.hexoadmin.entity.MarkDownFile;
 import top.itning.hexoadmin.service.FileService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 路由
  *
@@ -37,7 +40,19 @@ public class FrameController {
 
     @GetMapping("/newPost")
     public String newPost(Model model) {
-        model.addAttribute("file", new MarkDownFile());
+        String date = new SimpleDateFormat("yyyy-MM-dd-").format(new Date());
+        MarkDownFile markDownFile = new MarkDownFile();
+        markDownFile.setContent("---\n" +
+                "layout: post\n" +
+                "title: '  '\n" +
+                "comments: true\n" +
+                "date: " + date + "\n" +
+                "updated: " + date + "\n" +
+                "urlname: \n" +
+                "categories: \n" +
+                "tags: \n" +
+                "---");
+        model.addAttribute("file", markDownFile);
         return "editor";
     }
 
