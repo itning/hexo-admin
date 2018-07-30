d6fcc86e2d8d99711ed57f6e4d1c2cfcdd48f09a
itning <itning@itning.top>
2018-07-30 14:35:13 +0800

    delete test

--- src/test/java/top/itning/hexoadmin/HexoAdminApplicationTests.java
+++ src/test/java/top/itning/hexoadmin/HexoAdminApplicationTests.java
@@ -2,24 +2,14 @@ package top.itning.hexoadmin;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import top.itning.hexoadmin.entity.UserProps;
-import top.itning.hexoadmin.service.FileService;
-
-import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HexoAdminApplicationTests {
-    @Autowired
-    private FileService fileService;
-
-    @Autowired
-    private UserProps userProps;
 
     @Test
-    public void contextLoads() throws IOException, InterruptedException {
+    public void contextLoads() {
     }
 }
